704f89a99416a4046ab8fb7774da3170eb981590
obsoft23 <holarmidhey@gmail.com>
2022-10-10 16:58:55 +0100

    moved url business reviews

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -103,6 +103,7 @@ class ReviewController extends Controller
         ->select('users.id','users.name','users.image')
         ->join('reviews','reviewer_id','=','users.id')
         ->where(['business_account_id' => $id,])
+        ->orderBy("created_at", "DESC")
         ->get();
 
        $success = Review::where('business_account_id', '=', $id)->get();
@@ -118,6 +119,7 @@ class ReviewController extends Controller
         ->select('users.id','users.name','users.image')
         ->join('reviews','reviewer_id','=','users.id')
         ->where(['business_account_id' => $id,])
+        ->orderBy("created_at", "DESC")
         ->get();
 
 
